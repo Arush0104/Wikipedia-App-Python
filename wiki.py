31f3f3f4c26022e3b78c124ec5e25e1a09ef827c
procoder249 <50803990+procoder249@users.noreply.github.com>
2019-10-22 22:17:33 +0530

    added google search facility
    Now if the Script does not find what it's looking for in wikipedia, then it will perform a google search for the term and provide links to the user where the users can get what they are looking for.

--- wiki.py
+++ wiki.py
@@ -1,8 +1,21 @@
+from googlesearch import search
 import wikipedia
-while 1:
+
+global user 
+user = ''
+while (user != 'nothing'):
     try:
+        '''
+        wikipedia search for the input(user)
+        '''
         user = input("What do you want to search: ")
         wikipedia.set_lang("en")
         print(wikipedia.summary(user, sentences=2))
     except:
-        print("OOPS!.... I cant find what you want! Try something else.")
+        '''
+        google search for the input(user)
+        '''
+        print('''OOPS!.... I cant find what you want in Wikipedia! Try these links below, I'm sure you'll get it ''')
+        response = search(query=user,tld='co.in',lang='en',num=10,stop=1,pause=2)
+        for i in response:
+            print(i)
